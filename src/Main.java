3f36e77da009775acc957b3610c01c7ebcd94693
Dmitry Bychkov <yom.4100@gmail.com>
2022-10-19 07:12:40 +0300

    edit negative miles checking

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketCostAmount = 20;
+        int ticketCostAmount = -20;
         int milesAmount = ticketCostAmount/20;
 
-        if (milesAmount < 0) {
-            milesAmount = milesAmount * -1;
+        if (milesAmount <= 0) {
+            milesAmount = 0;
         }
 
         System.out.println("Количество начисленных миль: "+milesAmount);
